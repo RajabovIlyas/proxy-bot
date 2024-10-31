5bf16d43e8513e34fa8d0ea3332325735ce7fb72
RajabovIlyas <ilyasrajabow@gmail.com>
2024-10-31 17:03:19 +0500

    updated build command

--- internal/pkg/app/app.go
+++ internal/pkg/app/app.go
@@ -1,15 +1,22 @@
 package app
 
 import (
+	"fmt"
 	"github.com/RajabovIlyas/proxy-bot/config"
 	"github.com/RajabovIlyas/proxy-bot/internal/app/constants"
 	"github.com/RajabovIlyas/proxy-bot/internal/app/server"
 	"github.com/RajabovIlyas/proxy-bot/internal/pkg/telegram"
 	"github.com/rs/zerolog"
+	"os"
 )
 
 func Run(logger zerolog.Logger) error {
-	loadConfig, err := config.LoadConfig(constants.CONFIG_FILE_PATH)
+	nodeEnv := os.Getenv("NODE_ENV")
+	if nodeEnv == "" {
+		nodeEnv = "development"
+	}
+	configPath := fmt.Sprintf("%s.%s", constants.CONFIG_FILE_PATH, nodeEnv)
+	loadConfig, err := config.LoadConfig(configPath)
 
 	if err != nil {
 		logger.Fatal().Err(err).Msg("app.Run: load config error:")
